d76a3e8455ca7dfc4265a585bf1abc8e72c191f7
F9Uf <atoseka@gmail.com>
2019-12-27 23:45:43 +0700

    create toggle slide by number

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 var sliderItem = document.getElementById("slider-item");
 var sliderFooter = document.getElementById("slider-footer");
-var N = 5
+var prevTab = document.getElementById("prev");
 
+var N = 5
+var current = 1;
 
 for (let i = 0; i < N; i++) {
   var imgSrc = `./images/image${i+1}.jpg`;
@@ -11,24 +13,27 @@ for (let i = 0; i < N; i++) {
 
   var newDot = document.createElement("div");
   newDot.classList.add("dot")
+  newDot.addEventListener("click", () => {
+    return toggle(i+1)
+  });
   sliderFooter.appendChild(newDot);
 }
 
+sliderFooter.childNodes[current].classList.add("active");
 
-let i = 1;
-sliderFooter.childNodes[i].classList.add("active");
-
-function toggle (xx) {
-  console.log(xx);
-  if (i > N - 1) i = 0;
-  sliderItem.style.transform = `translateX(-${710 * i}px)`;
-  i += 1;
+function toggle (index) {
+  if (index > N ) index = 1;
+  if (index < 1) index = N;
+  console.log(index);
+  sliderItem.style.transform = `translateX(-${710 * (index - 1)}px)`;
+  current = index;
   for(let j = 1; j <= N; j++) {
-    if (j === i) {
-      sliderFooter.childNodes[i].classList.add("active");
+    if (j === current) {
+      sliderFooter.childNodes[current].classList.add("active");
     } else {
       sliderFooter.childNodes[j].classList.remove("active");
     }
   }
+  return true;
 }
 
